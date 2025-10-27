b5b0d259d1871c094fa0cb00d7912103b306b6f5
ypetu <yoelpetulla@hotmail.com>
2025-10-27 11:31:22 -0300

    con nosotros component

--- src/app/Components/nosotros/nosotros.component.ts
+++ src/app/Components/nosotros/nosotros.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './nosotros.component.html',
-  styleUrl: './nosotros.component.scss'
+  styleUrl: './nosotros.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NosotrosComponent {
 
 }
+
+
